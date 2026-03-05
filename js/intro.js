80fe754e160541c7f7ad8a1526cecb77bbd53077
Cesar <cessardevs@gmail.com>
2026-03-05 14:54:20 +0000

    Implement player guide at intro.html

--- js/intro.js
+++ js/intro.js
@@ -1,5 +1,5 @@
 const recruitmentForm = document.querySelector(".class-selection");
-
+//Player selection
 if (recruitmentForm) {
   recruitmentForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -15,3 +15,28 @@ if (recruitmentForm) {
     }, 500);
   });
 }
+
+ // Modal 
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("howToPlayModal");
+    const btn = document.getElementById("openModal");
+    const span = document.querySelector(".close-modal"); // Cambiado a querySelector
+
+    if (btn && modal && span) {
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    } else {
+        console.error("No se encontraron los elementos del modal. Revisa las clases en el HTML.");
+    }
+});
